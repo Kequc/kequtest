068f0cce88830530ff1cd02f4a71085cd0c22020
kequc <v2xyvxkr.1646bpmi@mail.getinbox.io>
2021-02-10 00:26:22 +0100

    Supports missing tests

--- test/main.test.js
+++ test/main.test.js
@@ -11,6 +11,31 @@ it('sets up test environment', function () {
     assert.strictEqual(typeof after, 'function');
 });
 
+describe('describe', function () {
+    it('throws an error when description is invalid', function () {
+        assert.throws(() => { describe(); }, { message: /^Description must be a string/ });
+        assert.throws(() => { describe(null); }, { message: /^Description must be a string/ });
+        assert.throws(() => { describe(100); }, { message: /^Description must be a string/ });
+    });
+    it('throws an error when callback is invalid', function () {
+        assert.throws(() => { describe('testing'); }, { message: /^Callback must be a function/ });
+        assert.throws(() => { describe('testing', null); }, { message: /^Callback must be a function/ });
+        assert.throws(() => { describe('testing', 100); }, { message: /^Callback must be a function/ });
+    });
+});
+
+describe('it', function () {
+    it('throws an error when description is invalid', function () {
+        assert.throws(() => { it(); }, { message: /^Description must be a string/ });
+        assert.throws(() => { it(null); }, { message: /^Description must be a string/ });
+        assert.throws(() => { it(100); }, { message: /^Description must be a string/ });
+    });
+    it('throws an error when callback is invalid', function () {
+        assert.throws(() => { it('testing', null); }, { message: /^Callback must be a function/ });
+        assert.throws(() => { it('testing', 100); }, { message: /^Callback must be a function/ });
+    });
+});
+
 it('runs test suite', async function () {
     const log = util.log();
     const absolute = __dirname + '/fake-src';
